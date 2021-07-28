d74219c9d271431f84193ee89247f1f45107d400
Pulkit Khandelwal <67990422+pulkitkhandelwal29@users.noreply.github.com>
2021-07-28 13:27:07 +0530

    Repopulate functionality in Slug field

--- category/admin.py
+++ category/admin.py
@@ -4,4 +4,10 @@ from .models import Category
 
 # Register your models here.
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    #Making Prepopulated field meaning name entering in category_name automatically should appear in slug field
+    prepopulated_fields = {'slug': ('category_name',)}
+    #Appears at front page of categories
+    list_display = ('category_name','slug')
+
+admin.site.register(Category,CategoryAdmin)
