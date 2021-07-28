a19561026aba98957f1a11f24106ccbc6ad8a89a
Pulkit Khandelwal <67990422+pulkitkhandelwal29@users.noreply.github.com>
2021-07-28 10:25:43 +0530

    Fixed app name in admin page of Django

--- category/models.py
+++ category/models.py
@@ -7,6 +7,11 @@ class Category(models.Model):
     description = models.TextField(blank=True)
     category_image = models.ImageField(upload_to = 'images/category')
 
+    #Name appearing on the admin page, it was 'Categorys', we renamed it to 'Categories'
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         '''__str__ is the name that is stored on admin page '''
         return self.category_name
